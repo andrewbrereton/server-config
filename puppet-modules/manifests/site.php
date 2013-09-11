cf06592c28487f5dc00c1e50d7f6b413ffc15519
Andrew Brereton <andrewbrereton@users.noreply.github.com>
2013-09-11 21:40:30 +1000

    Update site.php

--- puppet-modules/manifests/site.php
+++ puppet-modules/manifests/site.php
@@ -1,11 +1,21 @@
-package {
-    'apache2':
-        ensure => installed
+class { 'apache': }
+class { 'apache::mod::pagespeed':
+    domain_names => 'http://andrewbrereton.com https://andrewbrereton.com',
 }
-
-service {
-    'apache2':
-        ensure => true,
-        enable => true,
-        require => Package['apache2']
+class { 'apache::mod::php': }
+apache::mod { 'rewrite': }
+apache::vhost { 'andrewbrereton.com':
+    priority        => '10',
+    port            => '443',
+    template        => 'apache/vhost-symfony.conf.erb',
+    ssl             => true,
+    docroot         => '/var/www/andrewbrereton.com/web',
+    logroot         => '/var/log/apache2',
+    # Prevent directory listing by overriding options
+    options         => 'FollowSymLinks',
+    override        => 'All',
+    serveradmin     => 'webmaster@andrewbrereton.com',
+    serveraliases   => 'www.andrewbrereton.com',
+    docroot_owner   => 'www-data',
+    docroot_group   => 'www-data',
 }
