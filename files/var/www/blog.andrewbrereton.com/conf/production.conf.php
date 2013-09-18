88ad5118531c10dca715edaa77e7309c4cdeddd3
Andrew Brereton <andrewbrereton@users.noreply.github.com>
2013-09-18 10:24:50 +1000

    Create production.conf.php

---
+++ files/var/www/blog.andrewbrereton.com/conf/production.conf.php
@@ -0,0 +1,17 @@
+<?php
+
+	define('PERSONA_AUDIENCE', 'https://blog.andrewbrereton.com/');
+
+	define('SITE_TITLE', 'Blog | Andrew Brereton');
+	define('SITE_BASE_URL', 'https://blog.andrewbrereton.com/');
+
+	define('USER_EMAIL', 'andrew@andrewbrereton.com');
+	define('USER_NAME', 'Andrew Brereton');
+	define('USER_BIO', 'Web Developer');
+
+	define('DB_HOST', 'localhost');
+	define('DB_USER', 'blog');
+	define('DB_PASSWORD', 'SUPERSTRONGPASSWORD');
+	define('DB_DATABASE_NAME', 'blog');
+
+?>
